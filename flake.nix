9aab9c0daadfaac51abd62d5d266eca8612fedf0
Ember "n0emis" Keske <git@n0emis.eu>
2022-03-31 15:53:04 +0200

    q!

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     nixpkgs.url = "nixpkgs/nixos-21.11";
 
     pretixSrc = {
-      url = "github:pretix/pretix/v4.7.1";
+      url = "github:n0emis/pretix/patch-stripe-name";
       flake = false;
     };
   };
